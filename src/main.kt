3edd427b9079d48278807702b9df64895af0eddf
Oussaki <oussama.abdellah@gmail.com>
2017-10-16 18:19:49 +0400

    First init

---
+++ src/main.kt
@@ -0,0 +1,16 @@
+import javafx.application.Application
+
+fun main(args: Array<String>){
+    println("Kotlin Start")
+    Application.launch(CommonPool) {
+        delay(2000)
+        println("Kotlin Inside")
+    }
+    println("Kotlin End")
+
+}
+
+fun doSomething(){
+    delay(2000)
+
+}
