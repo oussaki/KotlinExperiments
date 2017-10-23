3fcf236c5d57627f687c8ba4ed6607c90958b3a1
Oussaki <oussama.abdellah@gmail.com>
2017-10-23 18:40:26 +0400

    Rest experiments

--- src/main/kotlin/springo/GreetingController.kt
+++ src/main/kotlin/springo/GreetingController.kt
@@ -1,10 +1,7 @@
 package springo
 
-import org.hibernate.validator.constraints.Email
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
-import springo.Greeting
-import java.net.Socket
 import java.util.concurrent.atomic.AtomicLong
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
@@ -27,13 +24,27 @@ class GreetingController {
         return User(counter.incrementAndGet().toInt(), name, email, phone)
     }
 
-
     @RequestMapping("user/add/{name}/{email}/{phone}")
     fun addUser(@PathVariable(name = "name") name: String, @PathVariable(name = "email") email: String,
                 @PathVariable(name = "phone") phone: String): User {
         var user = User(counter.incrementAndGet().toInt(), name, email, phone)
         userRepo.save(user)
+        val aUser = userRepo.findOne(1).copy()
+        print("User getted from db: ${aUser}")
+
         return user;
+    }
+
+
+    @RequestMapping("user/{id}")
+    fun findUser(@PathVariable(name = "id") id: Int): Any {
+        val user = userRepo!!.findOne(id)!!.copy()
+        print("User getted from db: ${user!!}");
+        if (user != null)
+            return user
+        else
+            return "user not found"
+
     }
 
 
@@ -43,6 +54,6 @@ class GreetingController {
 
 }
 
-internal interface UserRepository : CrudRepository<User, Long>
+internal interface UserRepository : CrudRepository<User, Int>
 
 @Entity data class User(@Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Int? = null, val name: String? = null, val email: String? = null, val phone: String? = null)
